b588107db80dc9bbbb70e5c728ef96fc8a07c813
Helen Efebe <efebehelen95@gmail.com>
2022-11-16 18:01:46 +0100

    Made progress

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,47 @@
 import React from "react";
+import "../Styles/StylesFooter.css"
 import Footerlogo from "../Images/footerlogo.png";
+import Facebook from "../Images/facebook.png";
+import Instagram from "../Images/instagram.png";
+import Twitter from "../Images/twitter.png";
+import Copyright from "../Images/copyright.png";
+
 
 const Footer =() => {
     return (
-        <div>
+        <div className="footer-container">
             <div>
-                <img src={ Footerlogo } alt="footerlogo" className="footerlogo" />
-                
+                <img src={ Footerlogo } alt="footerlogo" className="footerlogo footer-general" />
+                <ul  className="footer-general">
+                    <li><img src={Facebook} alt="facebook" /></li>
+                    <li><img src={Instagram} alt="instagram" /></li>
+                    <li><img src={Twitter} alt="twitter" /></li>
+                </ul>
+                <div className="copyright-section footer-general">
+                    <img src={Copyright} alt="copyright" />
+                    <p>2022 Metabnb</p>
+                </div>
+            </div>
+            <div className="footer-section footer-general">
+                <h3 className="footer-general">Community</h3>
+                <p className="footer-general">NFT</p>
+                <p className="footer-general">Tokens</p>
+                <p className="footer-general">Landlords</p>
+                <p className="footer-general">Discord</p>
+            </div>
+            <div className="footer-section">
+                <h3 className="footer-general">Places</h3>
+                <p className="footer-general">Castle</p>
+                <p className="footer-general">Farms </p>
+                <p className="footer-general">Beach</p>
+                <p className="footer-general">Learn more</p>
+            </div>
+            <div className="footer-section">
+                <h3 className="footer-general">About us</h3>
+                <p className="footer-general">Road map</p>
+                <p className="footer-general">Creators </p>
+                <p className="footer-general">Career</p>
+                <p className="footer-general">Contact us</p>
             </div>
         </div>
     )
--- src/Pages/PlaceToStay.js
+++ src/Pages/PlaceToStay.js
@@ -1,11 +1,282 @@
 import React from "react";
+import "../Styles/StylesHomePage.css";
+import Star from "../Images/star-frame.png";
+import Page2frame1 from "../Images/Frame1.png";
+import Page2frame2 from "../Images/Frame2.png";
+import Page2frame3 from "../Images/Frame3.png";
+import Page2frame4 from "../Images/Frame4.png";
+import Page2frame5 from "../Images/Frame5.png";
+import Page2frame6 from "../Images/Frame6.png";
+import Page2frame7 from "../Images/Frame7.png";
+import Page2frame8 from "../Images/Frame8.png";
+import Page2frame9 from "../Images/Frame9.png";
+import Page2frame10 from "../Images/Frame10.png";
+import Page2frame11 from "../Images/Frame11.png";
+import Page2frame12 from "../Images/Frame12.png";
+import Page2frame13 from "../Images/Frame13.png";
+import Page2frame14 from "../Images/Frame14.png";
+import Page2frame15 from "../Images/Frame15.png";
+import Page2frame16 from "../Images/Frame16.png";
 
 const PlaceToStay = () => {
-    return(
-        <div>
+  return (
+    <div>
+      <div className="display">
+        <p>Resturant </p>
+        <p>Cottage </p>
+        <p>Castle </p>
+        <p>fantast city </p>
+        <p>beach </p>
+        <p>Carbins </p>
+        <p>Off-grid </p>
+        <p>Farm </p>
+        <button className="display-button">Location </button>
+      </div>
+      <div className="grid-section-container">
+        <div className="grid-template">
+          <div className="frame-section">
+            <img src={Page2frame1} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
 
+          <div className="frame-section">
+            <img src={Page2frame2} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame3} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame4} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame5} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame6} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame7} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame8} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame9} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame10} alt="frame2" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame11} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame12} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame13} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame14} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame15} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="frame-section">
+            <img src={Page2frame16} alt="frame1" className="frame-images" />
+            <div className="below-image">
+              <div className="desert">
+                <p>Desert king</p>
+                <p>2345km away</p>
+                <img src={Star} alt="four-star" classNmae="star" />
+              </div>
+              <div className="available">
+                <p className="bold-text">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default PlaceToStay;
